efe53cc2ed7ccac5648284216cfa32b1b8167153
LinNes <lnestorson@gmail.com>
2021-10-18 11:28:21 +0200

    Added first functionality

---
+++ WordSearch/DocumentHandler/FileReader.cs
@@ -0,0 +1,32 @@
+﻿namespace WordSearch.DocumentHandler
+{
+    using System;
+    using System.IO;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class FileReader
+    {
+        //@"D:\Utbildning\Datalogi\Testning\BinaryTreeTest\cow.txt"
+        public string ReadFile(string path)
+        {
+            string fileString = File.ReadAllText(path);
+
+            return fileString;
+        }
+
+        public List<string> SplitLine(string fileText)
+        {
+            List<string> wordList = new List<string>();
+
+            string[] wordCollection = fileText.Split(' ');
+
+            foreach (var sub in wordCollection)
+            {
+                wordList.Add(sub);
+            }
+
+            return wordList;
+        }
+    }
+}
--- WordSearch/Program.cs
+++ WordSearch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace WordSearch
 {
@@ -6,7 +7,8 @@ namespace WordSearch
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("hej");
+            MenuHandler MenuPointer = new MenuHandler();
+            MenuPointer.Menu();
         }
     }
 }
