9b4cd9c847c958356d7aeab883596cd387e78d9e
LinNes <lnestorson@gmail.com>
2021-10-18 15:40:08 +0200

    Added Sorting functionality

--- WordSearch/MenuHandler.cs
+++ WordSearch/MenuHandler.cs
@@ -7,6 +7,7 @@
     using System.Text;
     using WordSearch.DocumentHandler;
     using WordSearch.Helpers;
+    using WordSearch.SearchHandlers;
     using WordSearch.Seeder;
 
     internal class MenuHandler
@@ -14,13 +15,15 @@
         private List<string> cowList = default;
         private List<string> chickenList = default;
         private List<string> sheepList = default;
+
         private string _cowPath = @"\cow.txt";
         private string _chickenPath = @"\Chicken.txt";
         private string _sheepPath = @"\Sheep.txt";
+        private InputHandler helperPointer = new InputHandler();
+        private SearchHandler searchPointer = new SearchHandler();
 
         public void Menu()
         {
-            InputHandler helper = new InputHandler();
             Seed();
             Console.WriteLine("Welcome!");
 
@@ -31,22 +34,59 @@
                 Console.WriteLine("2. Order document and search for x number of words");
                 Console.WriteLine("3. Print datastructure");
                 Console.WriteLine("4. Exit\n");
-                int userInput = helper.ParseUserInput();
+                int userInput = helperPointer.ParseUserInput();
                 switch (userInput)
                 {
                     case 1:
+
                         break;
+
                     case 2:
+                        ListChoice();
                         break;
+
                     case 3:
                         break;
+
                     case 4:
                         return;
+
                     default:
                         Console.WriteLine("Invaild option");
                         break;
                 }
             }
+        }
+
+        private void ListChoice()
+        {
+            List<string> sortedList = new List<string>();
+            Console.WriteLine("What document would you like to search?");
+            Console.WriteLine("1. cow.txt\n2. Chicken.txt\n3. Sheep.txt ");
+            int userInput = helperPointer.ParseUserInput();
+
+            switch (userInput)
+            {
+                case 1:
+                    sortedList = searchPointer.SortList(cowList);
+                    break;
+
+                case 2:
+                    sortedList = searchPointer.SortList(chickenList);
+                    break;
+
+                case 3:
+                    sortedList = searchPointer.SortList(sheepList);
+                    break;
+
+                default:
+                    Console.WriteLine("Invaild option");
+                    break;
+            }
+
+            Console.WriteLine("And how many words would you like to print?");
+            userInput = helperPointer.ParseUserInput();
+            searchPointer.WordCount(userInput, sortedList);
         }
 
         private void Seed()
---
+++ WordSearch/SearchHandlers/SearchHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace WordSearch.SearchHandlers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Linq;
+
+    internal class SearchHandler
+    {
+        public List<string> SortList(List<string> unOrdered)
+        {
+            unOrdered.Sort();
+            var sortedList = unOrdered;
+            return sortedList;
+        }
+
+        public void WordCount(int userInput, List<string> sortedList)
+        {
+            for (int i = 0; i < userInput; i++)
+            {
+                Console.WriteLine(sortedList[i]);
+            }
+        }
+    }
+}
