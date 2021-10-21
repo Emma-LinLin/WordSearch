4c837cfe0034b88bfa6a121a80ed54cd0bef66bc
LinNes <lnestorson@gmail.com>
2021-10-21 12:55:53 +0200

    Created class for document results

---
+++ WordSearch/DocumentHandler/DocumentController.cs
@@ -0,0 +1,21 @@
+﻿namespace WordSearch.DocumentHandler
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    internal class DocumentController
+    {
+        private List<Document> ListOfDocuments = new List<Document>();
+
+        public void AddItemToList(string fileName, string searchWord, int repeats)
+        {
+            ListOfDocuments.Add(new Document(fileName, searchWord, repeats));
+        }
+
+        public List<Document> GetList()
+        {
+            return ListOfDocuments;
+        }
+    }
+}
